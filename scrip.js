cdab00ed39b108b6a50b203f2c4e93952640735b
Ricardo07-uatx <richarvan07@gmail.com>
2025-05-01 13:15:46 -0600

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,5 +1,5 @@
 // Configuración - Reemplaza con tu URL de despliegue
-const API_URL = "TU_URL_APPS_SCRIPT";
+const API_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwzq82_7N02tY3TZHX5yhcTBZ0NwdzQZJhlnLxmhNchZ6EHT3VyY9ucPrKxdRaSGxTO2vRW7MVDH94/pubhtml";
 
 document.addEventListener('DOMContentLoaded', () => {
   loadProducts();
